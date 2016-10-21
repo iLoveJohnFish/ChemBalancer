bd92b7be727abe4d133206d73659feb6bbe01bcc
iLoveJohnFish <ranyodh_mandur@hotmail.com>
2016-10-20 22:12:10 -0400

    added comments about what i wanted to do with app

--- src/chembalancer/Capp.java
+++ src/chembalancer/Capp.java
@@ -3,7 +3,14 @@ package chembalancer;
 
 
 public final class Capp  {    
-    
+    /**
+     * This is the main class that will identify which classes and functions to use for different scenarios.
+     * for example: balancing regular equations, balancing half reactions, redox reactions,
+     * this app should also have info on each element, periodic table, options for visual appearance(color, layout), general info on chem like lewis diagrams n shit
+     * give attributes about the substances that were created by the reaction -- like reaction type, bond types, weight, color, etc.
+     * tell whether something is a diatomic,
+     * give a graph how something bonds
+     */
     
     //MAIN METHOD
     public static void main( String[] args ) {
